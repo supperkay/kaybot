bdc1f05a140586b6173e22efc337e4731e1d1f76
supperkay <khchkks@gmail.com>
2017-11-12 13:22:40 +0700

    initial...

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if (!is_null($events['events'])) {
 
             );
             // Query
-            $sql = sprintf("SELECT * FROM bots WHERE get='Hi'");
+            $sql = sprintf("SELECT * FROM bots WHERE get= :get");
             //$result = $connection->query($sql);
            
             error_log($sql);
